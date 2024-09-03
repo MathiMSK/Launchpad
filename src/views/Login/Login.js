4c2c884c4bf5946e8f36577fa5aaa266ea350805
Mathi <mskmthi@gmail.com>
2024-09-03 12:05:50 +0530

    ./

---
+++ src/views/Login/Login.js
@@ -0,0 +1,174 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col,
+} from "reactstrap";
+import "./login.css";
+import { auth, provider } from "./config/config";
+import { signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const Login = () => {
+  const [logValue, setLogValue] = useState();
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setLogValue(data);
+      localStorage.setItem("email:", data.user.email);
+    });
+  };
+
+  // useEffect(()=>{
+  //   setLogValue(localStorage.getItem("email"))
+  // })
+  return (
+    <div className="d-flex justify-content-center align-items-center loginCont">
+      <Col lg="4" md="6" sm="8" xs="12">
+        <Card className="bg-secondary shadow border-0">
+          <CardHeader className="bg-transparent">
+            <div className="header-body text-center">
+              <Row className="justify-content-center">
+                <Col>
+                  <h1 className="text">Welcome!</h1>
+                  <p className="text-lead text">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                </Col>
+              </Row>
+            </div>
+          </CardHeader>
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-muted text-center mt-2 mb-3">
+              <small>Sign in with</small>
+            </div>
+            <div className="btn-wrapper text-center">
+              <Button
+                className="btn-neutral btn-icon"
+                color="default"
+                onClick={handleClick}
+              >
+                <span className="btn-inner--icon">
+                  <img
+                    alt="Google"
+                    src={
+                      require("../../assets/img/icons/common/google.svg")
+                        .default
+                    }
+                  />
+                </span>
+                <span className="btn-inner--text">Google</span>
+              </Button>
+            </div>
+            {/* <div className="text-center text-muted mb-4">
+              <small>Or sign in with credentials</small>
+            </div>
+            <Form role="form">
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-email-83" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoComplete="new-email"
+                    bsSize="lg"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="new-password"
+                    bsSize="lg"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="custom-control custom-control-alternative custom-checkbox mb-3">
+                <input
+                  className="custom-control-input"
+                  id="customCheckLogin"
+                  type="checkbox"
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor="customCheckLogin"
+                >
+                  <span className="text-muted">Remember me</span>
+                </label>
+              </div>
+              <div className="text-center">
+                <Button className="my-4" color="primary" type="button">
+                  Sign in
+                </Button>
+              </div>
+            </Form> */}
+          </CardBody>
+        </Card>
+        {/* Uncomment and modify as needed for additional links */}
+        {/* <Row className="mt-3">
+          <Col xs="6">
+            <a
+              className="text-light"
+              href="#pablo"
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Forgot password?</small>
+            </a>
+          </Col>
+          <Col className="text-right" xs="6">
+            <a
+              className="text-light"
+              href="#pablo"
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Create new account</small>
+            </a>
+          </Col>
+        </Row> */}
+      </Col>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/views/Login/config/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth , GoogleAuthProvider } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ax80GACLGyNhOy6ZxdefeAyOWqPkxk8",
+  authDomain: "launchpad-cc20b.firebaseapp.com",
+  databaseURL: "https://launchpad-cc20b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "launchpad-cc20b",
+  storageBucket: "launchpad-cc20b.appspot.com",
+  messagingSenderId: "614615091187",
+  appId: "1:614615091187:web:d04be665fe27ab3e55b486",
+  measurementId: "G-D2CETVEHBS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider();
+export {auth,provider}
+
+
+
